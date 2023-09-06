2b9c9ead07509e9acac1cadfcfae65dcf7ad5f9d
ambialy <ambialy@buffalo.edu>
2023-09-05 21:57:28 -0400

    added date stuff

--- gui.py
+++ gui.py
@@ -1,20 +1,37 @@
 import tkinter as tk
 from tkinter import ttk
+import tkcalendar
+from datetime import datetime
 
 root = tk.Tk()
 root.title("Rocket Budgeting")
 
-for i in range(6):
-    root.columnconfigure(i, weight=1)
-    root.rowconfigure(i, weight=i)
+# frame = ttk.Frame(root)
 
-    for j in range(4):
-        frame = tk.Frame(master=root)
-        frame.grid(row=i, column=j, padx=5, pady=5)
-        label = tk.Label(master=frame, text=f"Row {i}\nColumn {j}")
-        label.pack(padx=5, pady=5)
+# want a 6x4 grid window
 
+lbl_transaction = ttk.Label(root, text="Add a new transaction")
+# lbl_transaction.grid(row=0, column=0, columnspan=4)
+lbl_transaction.pack(pady=10)
 
+style = ttk.Style()
+style.theme_use('clam')  # -> uncomment this line if the styling does not work
+
+# Old calendar stuff
+cal = tkcalendar.Calendar(root, selectmode='day', year=datetime.today().year,
+                month=datetime.today().month, day=datetime.today().day)
+cal.pack()
+
+def grab_date():
+    lbl_date.config(text=cal.get_date())
+
+butt_date = tk.Button(root, text="Select date", command=grab_date)
+butt_date.pack()
+lbl_date = tk.Label(root, text='')
+
+amountVar = tk.DoubleVar()
+entry_amount = tk.Entry(root, textvariable=amountVar)
+entry_amount.pack(pady=5)
 
 # ***** Menubar stuff *********
 menubar = tk.Menu(root)
